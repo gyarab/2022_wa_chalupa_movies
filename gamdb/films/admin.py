c7cdbb772924224eb77d6cb3fe535308dce043e6
Petr Chalupa <petr.chalupa@student.gyarab.cz>
2023-03-21 11:16:45 +0100

    director, genre

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Director, Genre
 
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "year"]
+    list_display = ["name", "year", "director", "genres_display"]
     list_display_links = ["name"]
+    search_fields = ["name"]
+    list_filter = ["genres"]
 
 
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(Director)
+admin.site.register(Genre)
