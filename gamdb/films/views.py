c0ef1a7c8d71c55c0dc5d136f81873ac439df78b
Petr Chalupa <petr.chalupa@student.gyarab.cz>
2023-04-04 11:17:41 +0200

    urls

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Movie, Director
 
-# Create your views here.
+
+def directors(request):
+    context = {
+        "logic": True,
+        "title": "Nejoblíbenější režiséři",
+        "directors": Director.objects.all()
+    }
+    return render(request, "directors.html", context)
+
+
+def homepage(request):
+    context = {
+        "movies": Movie.objects.all()
+    }
+    return render(request, "index.html", context)
