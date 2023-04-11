c47ae793b7aba37ffe1f7cf53d0d76962c00d7e0
Petr Chalupa <petr.chalupa@student.gyarab.cz>
2023-04-11 11:18:54 +0200

    redukce

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from .models import Movie, Director
 
 
+def homepage(request):
+    context = {
+        "movies": Movie.objects.all()
+    }
+    return render(request, 'homepage.html', context)
+
+
 def directors(request):
     context = {
         "logic": True,
@@ -11,8 +18,8 @@ def directors(request):
     return render(request, "directors.html", context)
 
 
-def homepage(request):
+def movies(request):
     context = {
         "movies": Movie.objects.all()
     }
-    return render(request, "index.html", context)
+    return render(request, "movies.html", context)
--- gamdb/gamdb/urls.py
+++ gamdb/gamdb/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from films.views import homepage, directors
+from films.views import homepage, movies, directors
 
 urlpatterns = [
     path("", homepage, name="homepage"),
+    path("movies/", movies, name="movies"),
     path("directors/", directors, name="directors"),
     path("admin/", admin.site.urls),
 ]
